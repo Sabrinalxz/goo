26bc01da95effe4ff1354b049f7072f659384f2d
Sabrina <xinzhul@mtu.edu>
2023-12-11 16:39:10 -0500

    new version

--- dostats.sh
+++ dostats.sh
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-cd north-pacific-gyree
-=======
 cd north-pacfic-gyre
->>>>>>> 8f034230d865506d4aed3f7799f109db4a4d3651
 for f in NENE*[AB].txt; do echo ; bash goostats.sh  stats-; done
